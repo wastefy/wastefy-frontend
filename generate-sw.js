4869d32aabcb1f37772317eb21b4505b3013d720
graciaaurelya <graciaaurelyaa30@gmail.com>
2026-06-03 18:28:52 +0800

    fix: auto-create public folder before generating sw

--- generate-sw.js
+++ generate-sw.js
@@ -21,5 +21,6 @@ messaging.onBackgroundMessage((payload) => {
   })
 })`;
 
+fs.mkdirSync('./public', { recursive: true });
 fs.writeFileSync('./public/firebase-messaging-sw.js', content);
 console.log('✅ firebase-messaging-sw.js generated!');
